4211c8583c5a42c889b377f6f38550250a1c5541
Farid Attamimi <97972332+Tamimi2022@users.noreply.github.com>
2022-03-25 17:33:35 +0100

    Add files via upload

---
+++ RunJs/script.js
@@ -0,0 +1,21 @@
+// Part 1
+console.log("Hello Winc Academy students")
+
+// Part 2
+// "This is a large sum"
+let calcu = (1230941 * 1823794);
+console.log(calcu);
+
+let calcu1 = (637263 / 54);
+console.log(calcu1);
+
+// Answer no.4 No, because we use command for text comment "//"
+
+/* Answer no.5 
+let calcu = (1230941 * 1823794);
+console.log(calcu);
+
+let calcu1 = (637263 / 54);
+console.log(calcu1); */
+
+// Answer no.6 No, because we use command "/*  */"
